492ab8c08561293cb5d2ca9b049619f99ee3a401
ceesiyamoah <ccyamoah@gmail.com>
2020-11-23 02:06:22 +0000

    redux devtools removal

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from './App';
 import reducer from './components/reducers';
 
-
-const store = createStore(
-	reducer,
-	applyMiddleware(reduxThunk)
-);
+const store = createStore(reducer, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
 	<Provider store={store}>
